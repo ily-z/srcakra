edcee0fd03245150e34436f5c2b614cd7e0fa72d
Muhammad Ilyas Zaini <qiyas.iq@gmail.com>
2026-04-09 00:37:24 +0700

    re push 1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,11 +39,11 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @viteReactRefresh
+        {{-- @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-        @inertiaHead
+        @inertiaHead --}}
     </head>
     <body class="font-sans antialiased">
-        @inertia
+        {{-- @inertia --}}
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+Route::get('/', function () {
+    return redirect('/booking');
 });
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+// });
 
-Route::get('/booking', [ReservationController::class, 'create']);
-Route::post('/reservations', [ReservationController::class, 'store']);
+// Route::get('/booking', [ReservationController::class, 'create']);
+// Route::post('/reservations', [ReservationController::class, 'store']);
 
 require __DIR__.'/settings.php';
